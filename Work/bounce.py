a54ff801b63c8fe8e6676349e95d1887cf3e5759
Julia Elisa <hanksdaniels@gmail.com>
2024-06-25 04:56:48 -0300

    1.3 exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,26 @@
 # bounce.py
 #
 # Exercise 1.5
+
+
+
+#######  Usando for 
+
+#h = 100 #meters
+#b = 0.6*h
+#
+#for i in range(10):
+#    print(i, round(b, 4))
+#    b = (0.6)*b
+   
+
+####### Usando while
+
+h = 100
+b = 1
+
+while b <= 10:
+    h = 0.6*h
+    print(b, round(h, 4))
+    b = b + 1
+    
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,37 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+total = 500000.0
+anual_rate = 0.05
+montly_rate = anual_rate/12
+payment = 2684.11
+total_paid = 0.0
+total_months = 0
+
+extra_payment = 1000
+extra_payment_startmonth = 61
+extra_payment_endmonth = 108
+
+while total > 0: #loop continua até o total a ser pago zerar
+    total_months = total_months + 1 
+    interest = total * (montly_rate) 
+    total = total + interest     #atualiza o total a ser pago
+      
+    #Applying extra payment 
+    if total_months >= extra_payment_startmonth and total_months <= extra_payment_endmonth: 
+        total = total - extra_payment   #desconta o pagamento extra do total a ser pago
+        total_paid = total_paid + extra_payment #adiciona o pagamento extra no valor pago 
+     
+     #Check if the remaing principal is less than regular payment    
+    if total < payment: #se o total a ser pago for menor que o payment     
+        payment = total
+        
+    total = total - payment
+    total_paid = total_paid + payment   #atualiza o quanto já foi pago a cada mês
+           
+    print(total_months, round(total_paid, 2), round(total, 2))  
+    
+    
+print('Total paid:', round(total_paid, 2))
+print('Total Months:', total_months)
