00346ed338a351570b3f9ab58259ddcea00625ec
Julia Elisa <hanksdaniels@gmail.com>
2024-08-08 22:31:17 -0300

    2.1 notes

---
+++ Work/notes/2.1.py
@@ -0,0 +1,9 @@
+#2.1 Datatypes and Data structures
+
+x = 'GOOG', 100, 490.1
+
+print(type(x)) #usually a collection of distinct items, usually all of the same type.
+
+name, shares, price = x
+
+print(name, shares, price)
